4577c9ea50e6368fe2f138a4af2813c16b2c0b52
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-11 04:06:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function happyHolidays(string) {return "Happy holidays!"}
 function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`;
 }
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday},${holiday},${name}!`;
+}
